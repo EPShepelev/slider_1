25fa99c7342eebe1aefd5fd087deb14addf9bfc4
Evgeniy <shepelev.ep@yandex.ru>
2019-07-07 21:47:45 +0300

    Add constructor
    For slider add constructor  method

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,3 @@
-sliderOne.init();
+const container = document.querySelector('#slider1'); //get html 
+const slider_1 = sliderConstruct.createNewSlider(); //create new slider
+slider_1.init(container); //set html 'container' to slider
